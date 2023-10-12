67d8704264fd921a52c3080e68a598acc34fb74f
timothy <jsbibtimothy@itcom888.com>
2023-10-12 18:14:04 +0800

    code commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# GoTeam
+# GoTeam Quiz 2
+
+# to run the code >>
+# use terminal
+# run:
+# cd dashboarduiux
+# npm i
+# npm run serve
---
+++ dashboarduiux/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <navBar/>
+  <div class="main-body">
+    <sideBar/>
+    <div class="content-main">
+      <contentMain/>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+import navBar from './components/navBar.vue'
+import sideBar from './components/sideBar.vue'
+import contentMain from './components/contentMain.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navBar,
+    sideBar,
+    contentMain
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 14px;
+  color: #474b54;
+  background: #f5f9fd;
+}
+.main-body {
+  margin-left: 256px;
+  margin-right: 280px;
+}
+.content-main {
+  margin-left: 160px;
+  margin-top: 20px;
+}
+</style>
